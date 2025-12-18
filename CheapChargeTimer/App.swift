92dc93aeba89f7124b48b16c2d2709ecfee72e90
Alex Harris <alexharris0079@gmail.com>
2025-12-19 06:44:54 +1000

    project: add initial SwiftUI scaffold

---
+++ CheapChargeTimer/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct CheapChargeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheapChargeTimer/ContentView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Cheap Charge Timer")
+            .padding()
+    }
+}
